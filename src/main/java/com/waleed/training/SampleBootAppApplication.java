a26593d2512bee866a9fe4aedddfb162eaba78f1
Saurabh <saurabh.d2106@gmail.com>
2023-12-05 10:32:27 +0400

    Adding Jenkinsfile

---
+++ src/main/java/com/waleed/training/SampleBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.waleed.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waleed/training/SampleControllerTest.java
@@ -0,0 +1,31 @@
+package com.waleed.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SampleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+               .andExpect(status().isOk())
+               .andExpect(content().string("Hello, World!"));
+    }
+
+    @Test
+    void testSquareEndpoint() throws Exception {
+        mockMvc.perform(get("/api/square?number=5"))
+               .andExpect(status().isOk())
+               .andExpect(content().string("25"));
+    }
+}
